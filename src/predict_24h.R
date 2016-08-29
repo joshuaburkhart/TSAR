98f9b4df3a2741ed9184788f920de7643ab08b23
Joshua Burkhart Nyx <burkhart.joshua@gmail.com>
2016-08-29 11:50:25 -0700

    adding 24h predictions

--- src/predict_24h.R
+++ src/predict_24h.R
@@ -16,25 +16,34 @@ library(e1071)
 print("globals")
 IMAGES_AS_PNG <- TRUE
 DATA_DIR <- "/media/burkhart/D052-7853/McWeeney/TSAR/"
-PHAS1_CLINICL <- paste(DATA_DIR,"ViralChallenge_test_Phase1_CLINICAL.tsv",sep="")
-PHAS1_EXPRESS <- paste(DATA_DIR,"ViralChallenge_test_Phase1_EXPRESSION_RMA.tsv",sep="")
+PHAS3_CLINICL <- paste(DATA_DIR,"ViralChallenge_test_Phase3_CLINICAL.tsv",sep="")
+PHAS3_EXPRESS <- paste(DATA_DIR,"ViralChallenge_test_Phase3_EXPRESSION_RMA.tsv",sep="")
 
 print("misc_config")
 setwd(DATA_DIR)
 
+print("helper functions")
+filter_24h_times <- function(x){
+  x %>% dplyr::filter(TIMEHOURS == 0 | # all studies
+                        TIMEHOURS == 21.5 | # DEE1, 2, 3, 4X, 
+                        TIMEHOURS == 18 | # DEE5
+                        TIMEHOURS == 24) %>% # Rhinovirus Duke, UVA
+    return()
+}
+
 print("load features")
-load("/home/burkhart/Software/TSAR/src/top_2000.rda") #loads top_2000 from train_0h
+load("/home/burkhart/Software/TSAR/src/top_2000.rda") #loads top_2000 from train_24h
 
 print("load trained model")
-load("/home/burkhart/Software/TSAR/src/tunedModel.rda") #loads trained_svm from train_0h
+load("/home/burkhart/Software/TSAR/src/tunedModel.rda") #loads trained_svm from train_24h
 
-print("load phas1 clinical data")
-phas1_clinicl_df <- read.table(PHAS1_CLINICL, header = T, sep = "\t")
+print("load phas3 clinical data")
+phas3_clinicl_df <- read.table(PHAS3_CLINICL, header = T, sep = "\t")
 
-print("load phas1 eset")
+print("load phas3 eset")
 rma_exprs <- as.matrix(
   read.table(
-    PHAS1_EXPRESS,
+    PHAS3_EXPRESS,
     header = TRUE,
     sep = "\t",
     row.names = 1,
@@ -44,36 +53,36 @@ rma_exprs <- as.matrix(
 )
 
 print("keep only t<0 arrays")
-tlt0_cel <- phas1_clinicl_df %>%
-  dplyr::filter(TIMEHOURS < 0) %>%
+ta24_cel <- phas3_clinicl_df %>%
+  filter_24h_times() %>%
   dplyr::mutate(CEL = as.character(CEL)) %>%
   dplyr::select(CEL)
 
-tlt0_exprs <- rma_exprs[, tlt0_cel$CEL]
+ta24_exprs <- rma_exprs[, ta24_cel$CEL]
 
-rma_eset <- ExpressionSet(assayData = tlt0_exprs)
+rma_eset <- ExpressionSet(assayData = ta24_exprs)
 
 print("keep only training features")
-tlt0_exprs <- rma_eset[top_2000$probesetid, ] %>%
+ta24_exprs <- rma_eset[top_2000$probesetid, ] %>%
   exprs() %>%
   as.data.frame() %>%
-  dplyr::select(match(phas1_clinicl_df %>%
-                        dplyr::filter(TIMEHOURS < 0) %>%
+  dplyr::select(match(phas3_clinicl_df %>%
+                        filter_24h_times() %>%
                         dplyr::mutate(CEL = as.character(CEL)) %>%
                         dplyr::select(CEL) %>% .[,1],names(.)))
 
-phas1_data <- tlt0_exprs %>%
+phas3_data <- ta24_exprs %>%
   t() %>%
   as.data.frame() %>%
   dplyr::add_rownames(var = "CEL") %>%
   dplyr::select(-CEL)
 
-print("predict phas1 scores")
-phas1_predictions <- predict(tunedModel,phas1_data) %>% as.numeric()
+print("predict phas3 scores")
+phas3_predictions <- predict(tunedModel,phas3_data) %>% as.numeric()
 
-print("store phas1 predictions")
-write.table(phas1_predictions,
-            file="/home/burkhart/Software/TSAR/src/phas1_predictions.csv",
+print("store phas3 predictions")
+write.table(phas3_predictions,
+            file="/home/burkhart/Software/TSAR/src/phas3_predictions.csv",
             sep=",")
 
 
