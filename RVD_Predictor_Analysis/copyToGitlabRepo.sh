c2644485ecad02d4d3f25cd6fe787edb982af98b
Joshua Burkhart Nyx <burkhart.joshua@gmail.com>
2017-08-10 12:21:37 -0700

    updating copier

--- RVD_Predictor_Analysis/copyToGitlabRepo.sh
+++ RVD_Predictor_Analysis/copyToGitlabRepo.sh
@@ -1,4 +1,5 @@
 cp -R ./data ../../subproject1/
 cp -R ./prototype ../../subproject1/
 cp -R ./src ../../subproject1/
-cp -R ./config/README.txt
+cp -R ./config/README.txt ../../subproject1/config/
+cp -R ./.gitignore ../../subproject1/
