30431e4aaf91e059841264ff3d22d1351b4da495
Joshua Burkhart <burkhart.joshua@gmail.com>
2017-08-15 23:27:46 -0700

    editing copy script

--- RVD_Predictor_Analysis/copyToGitlabRepo.sh
+++ RVD_Predictor_Analysis/copyToGitlabRepo.sh
@@ -1,11 +1,13 @@
 rm -rf ../../subproject1/data
 rm -rf ../../subproject1/prototype
 rm -rf ../../subproject1/src
+rm -rf ../../subproject1/results
 rm -rf ../../subproject1/config/README.txt
 rm -rf ../../subproject1/.gitignore
 
 cp -R ./data ../../subproject1/
 cp -R ./prototype ../../subproject1/
 cp -R ./src ../../subproject1/
+cp -R ./results ../../subproject1/
 cp -R ./config/README.txt ../../subproject1/config/
 cp -R ./.gitignore ../../subproject1/
