86b3a92ce4f356df0edb6bcaf1ea2ebe622dfaea
Joshua Burkhart Nyx <burkhart.joshua@gmail.com>
2017-08-28 20:32:57 -0700

    adding notes for predictor list justification

--- RVD_Predictor_Analysis/src/Subchallenge1Report.R
+++ RVD_Predictor_Analysis/src/Subchallenge1Report.R
@@ -8,6 +8,16 @@
 source("../data/Download.R")
 
 # calculate genesets
+## all predictor lists from teams that separated predictors by virus
+## including teams who only participated in subchallenge 1
 source("./per_study/PerStudyAnalysis.R")
+
+## all predictor lists from teams that had both timepoints from at least
+## one of the challenges with significant results (sub2 and sub3) with
+## less than all the predictors and valid, parsable files
 source("./per_timepoint/PerTimepointAnalysis.R")
+
+## all predictors lists fom teams that had both timepoints from both
+## of the challenges with significant results (sub2 and sub3) with
+## less than all the predictors and valid, parsable files
 source("./per_subchallenge/PerSubchallengeAnalysis.R")
