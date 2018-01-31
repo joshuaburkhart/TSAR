7fa439d465294fc7e0acd1255a23cc3ec49e30c5
Joshua Burkhart Nyx <burkhart.joshua@gmail.com>
2018-01-31 12:53:02 -0800

    cleaning up gene intersection output

--- RVD_Predictor_Analysis/src/compareGenelistsFromLiterature.R
+++ RVD_Predictor_Analysis/src/compareGenelistsFromLiterature.R
@@ -5,12 +5,18 @@ library(hgu133a2.db)
 library(GeneOverlap)
 
 #globs
-DATA_DIR <- "/home/burkhart/Software/TSAR/RVD_Predictor_Analysis/data/genelists/"
+SRC_DIR <- "/home/burkhart/Software/TSAR/RVD_Predictor_Analysis/"
+DATA_DIR <- paste(SRC_DIR,"data/genelists/",sep="")
+OUT_DIR <- paste(SRC_DIR,"results/GeneLists/",sep="")
 RVD_DIR <- paste(DATA_DIR,"RVD/",sep="")
 LIT_DIR <- paste(DATA_DIR,"LIT/",sep="")
-OVERLAP_DATA_TABLE_FN <- paste(DATA_DIR,"LitGeneSetOverlapSignificance.csv",sep="")
-OVERLAP_LISTS_FN <- paste(DATA_DIR,"LitGeneSetIntersection.csv",sep="")
-GENE_UNIVERSE_SIZE <- as.list(hgu133a2SYMBOL[hgu133a2SYMBOL %>% mappedkeys()]) %>% unlist(use.names = FALSE) %>% unique() %>% length()
+OVERLAP_DATA_TABLE_FN <- paste(OUT_DIR,"LitGeneSetOverlapSignificance.csv",sep="")
+OVERLAP_LISTS_FN <- paste(OUT_DIR,"LitGeneSetIntersection.csv",sep="")
+GENE_UNIVERSE_SIZE <- as.list(hgu133a2SYMBOL[hgu133a2SYMBOL %>% #12434
+                                               mappedkeys()]) %>%
+  unlist(use.names = FALSE) %>%
+  unique() %>%
+  length()
 
 #load RVD Challenge gene lists (single columns without headers)
 rvd_filenames <- list.files(RVD_DIR, pattern="*.csv", full.names=TRUE)
@@ -30,7 +36,7 @@ for(filename1 in rvd_filenames){
 unique_lit_data <- list()
 for(filename2 in lit_filenames){
   for(column in colnames(lit_data[[filename2]])){
-    unique_lit_data[[paste(basename(filename2),column,sep=",")]] <- 
+    unique_lit_data[[paste("LIT_SOURCE(",basename(filename2),")-LIT_GENELIST(",column,")",sep="")]] <- 
       unique(toupper(lit_data[[filename2]][,column]))
   }
 }
@@ -39,7 +45,7 @@ for(filename2 in lit_filenames){
 overlap_results <- list()
 for(element1 in names(unique_rvd_data)){
   for(element2 in names(unique_lit_data)){
-    overlap_results[[paste(element1,element2,sep=",")]] <- 
+    overlap_results[[paste("RVD_GENELIST(",element1,")-",element2,sep="")]] <- 
       newGeneOverlap(
         unique_rvd_data[[element1]], # List A
         unique_lit_data[[element2]], # List B
@@ -68,7 +74,11 @@ for(element in names(overlap_results)){
   
   intersection_list[[element]] <- c(element,getIntersection(gene_overlap))
 }
+
+# BH adjusted p
 bh_adj_p <- p.adjust(fisher_exact_p,method="BH")
+
+# write intersection significances
 data.frame(Comparison = genelist_comparison,
            Num.Neither = contingency_table_notB_notA,
            Num.RVD.Only = contingency_table_notB_inA,
@@ -76,8 +86,9 @@ data.frame(Comparison = genelist_comparison,
            Num.Both = contingency_table_inB_inA,
            Fisher.Exact.P = fisher_exact_p,
            BH.Adj.P = bh_adj_p) %>%
-  write.csv(file=OVERLAP_DATA_TABLE_FN)
+  write.csv(file=OVERLAP_DATA_TABLE_FN, row.names = FALSE)
 
-writeLines(unlist(lapply(intersection_list, paste, collapse=" ")),con = OVERLAP_LISTS_FN)
+# write gene intersection lists
+writeLines(unlist(lapply(intersection_list, paste, collapse=",")),con = OVERLAP_LISTS_FN)
 
 
